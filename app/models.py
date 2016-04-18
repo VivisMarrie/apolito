0a3d6e1fa9ed81b24e2c164aa06620cbf0ec25d5
VivisMarrie <viviane@s1mbi0se.com.br>
2016-04-18 00:50:07 -0300

    Added files via upload

---
+++ app/models.py
@@ -0,0 +1,45 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+
+
+class Toques(models.Model):
+    x = models.IntegerField() # ponto x
+    y = models.IntegerField() # ponto y
+    t = models.IntegerField() # tempo
+    def __str__(self):
+        return "x: " + self.x + ", y:" + self.y + ", t:" + self.t
+
+class Jogo(models.Model):
+    fase = models.IntegerField()
+    tempo = models.IntegerField()
+    tentativas = models.IntegerField()
+    frustrado = models.BooleanField()
+    toques = models.ManyToManyField(Toques)
+    def __str__(self):
+        return self.fase
+
+    '''{
+        "fase": 1,
+        "tempo": 100,
+        "tentativas": 2,
+        "toques": [
+            {
+                "x": 100,
+                "y": 200,
+                "t": 2
+            },
+            {
+                "x": 500,
+                "y": 300,
+                "t": 6
+            },
+            {
+                "x": 400,
+                "y": 800,
+                "t": 8
+            }
+        ],
+        "frustrado": false
+    }'''
---
+++ app/views.py
@@ -0,0 +1,25 @@
+import json
+from app.models import Toques, Jogo
+from django.http import HttpResponse, request
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def index(request):
+    if request.method == 'POST':
+        print request.body
+        json_data = json.loads(request.body.decode("utf-8"))
+        g = Jogo()
+        g.fase = json_data["fase"]
+        g.tempo = json_data["tempo"]
+        g.tentativas = json_data["tentativas"]
+        g.frustrado = json_data["frustrado"]
+        g.save()
+        for i in json_data["toques"]:
+            t = Toques()
+            t.x = i["x"]
+            t.y = i["y"]
+            t.t = i["t"]
+            t.save()
+            g.toques.add(t)
+        return HttpResponse(json_data)
