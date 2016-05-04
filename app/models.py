b6f82cf05065a14c9f8cf7f8961ec2f0e6baa4c3
Viviane Ribeiro de Sousa <viviane@s1mbi0se.com.br>
2016-05-04 20:37:25 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -8,13 +8,13 @@ class Toques(models.Model):
     x = models.IntegerField() # ponto x
     y = models.IntegerField() # ponto y
     t = models.IntegerField() # tempo
-	acao = models.IntegerField() # qual acao sera executada nesse clique
+    acao = models.IntegerField() # qual acao sera executada nesse clique
     def __str__(self):
         return "x: " + self.x + ", y:" + self.y + ", t:" + self.t
 
 class Jogo(models.Model):
     fase = models.IntegerField()
-	aluno = models.IntegerField()
+    aluno = models.IntegerField()
     tempo = models.IntegerField()
     tentativas = models.IntegerField()
     frustrado = models.BooleanField()
@@ -24,7 +24,7 @@ class Jogo(models.Model):
 
     '''{
         "fase": 1,
-		"aluno": 1;
+	"aluno": 1;
         "tempo": 100,
         "tentativas": 2,
         "toques": [
@@ -32,19 +32,19 @@ class Jogo(models.Model):
                 "x": 100,
                 "y": 200,
                 "t": 2,
-				"acao" : 0
+		"acao" : 0
             },
             {
                 "x": 500,
                 "y": 300,
                 "t": 6,
-				"acao" : 2
+		"acao" : 2
             },
             {
                 "x": 400,
                 "y": 800,
                 "t": 8,
-				"acao" : 4
+		"acao" : 4
             }
         ],
         "frustrado": false
