a9d2c1038dc2163bbfa6440e8c5a40acd87bef48
Viviane Ribeiro de Sousa <viviane@s1mbi0se.com.br>
2016-05-04 20:37:46 -0300

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-	url(r'^get_aluno$', views.get_aluno, name='get_aluno'),
+    url(r'^get_aluno$', views.get_aluno, name='get_aluno'),
 ]
