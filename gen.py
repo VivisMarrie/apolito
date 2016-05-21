b950d859fede4dc432e2b1a9e87660380702dcba
Viviane Ribeiro de Sousa <viviane@s1mbi0se.com.br>
2016-05-21 01:20:56 -0300

    Update gen.py

--- gen.py
+++ gen.py
@@ -6,8 +6,8 @@ from django.db.models.deletion import Collector
 
 from app.models import Jogo
 
-collector = NestedObjects(using="apolito") # database name
-collector.collect([Jogo.objects.get(pk=1)])
+collector = NestedObjects(using="default") # database name
+collector.collect(list(Jogo.objects.all()))
 
 objects = list(chain.from_iterable(collector.data))
 with open("backup_export.json", "w") as f:
