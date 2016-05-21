25422e14d4b5e3e84d873e8b3bb02f489d7a1a82
Viviane Ribeiro de Sousa <viv.sousa@hotmail.com>
2016-05-21 15:26:06 -0300

    changes

--- app/admin.py
+++ app/admin.py
@@ -3,5 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from models import Jogo, Toques
 
-admin.site.register(Jogo)
+#admin.site.register(Jogo)
 admin.site.register(Toques)
+
+class ToquesInline(admin.TabularInline):
+    model = Toques
+
+class JogoAdmin(admin.ModelAdmin):
+    inlines = [
+        ToquesInline,
+    ]
+
+admin.site.register(Jogo, JogoAdmin)
