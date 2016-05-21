d9313ed989c757b346a6c2a401fc1c44281a85bd
Viviane Ribeiro de Sousa <viviane@s1mbi0se.com.br>
2016-05-21 12:54:36 -0300

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,4 +1,11 @@
-from itertools import chain      
+from django.core import serializers
+from app import models
+data = serializers.serialize("json", models.Jogo.objects.filter(fase=1,aluno=1))
+out = open("mymodel.json", "w")
+out.write(data)
+out.close()
+
+'''from itertools import chain      
 
 from django.core import serializers
 from django.contrib.admin.utils import NestedObjects
@@ -12,3 +19,4 @@ collector.collect([Jogo.objects.get(pk=1)])
 objects = list(chain.from_iterable(collector.data))
 with open("backup_export.json", "w") as f:
     f.write(serializers.serialize("json", objects))
+'''
