bf02304277f076e9d2f657a4876b379367a7ad60
Viviane Ribeiro de Sousa <viv.sousa@hotmail.com>
2016-05-21 15:38:24 -0300

    Revert "changes"
    This reverts commit 25422e14d4b5e3e84d873e8b3bb02f489d7a1a82.

--- app/admin.py
+++ app/admin.py
@@ -3,15 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from models import Jogo, Toques
 
-#admin.site.register(Jogo)
+admin.site.register(Jogo)
 admin.site.register(Toques)
-
-class ToquesInline(admin.TabularInline):
-    model = Toques
-
-class JogoAdmin(admin.ModelAdmin):
-    inlines = [
-        ToquesInline,
-    ]
-
-admin.site.register(Jogo, JogoAdmin)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,16 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
+
+class Toques(models.Model):
+    x = models.IntegerField() # ponto x
+    y = models.IntegerField() # ponto y
+    t = models.IntegerField() # tempo
+    acao = models.IntegerField() # qual acao sera executada nesse clique
+    def __str__(self):
+        return "x: " + self.x.__str__() + ", y:".__str__() + self.y.__str__() + ", t:" + self.t.__str__()
 
 class Jogo(models.Model):
     fase = models.IntegerField()
@@ -6,19 +18,9 @@ class Jogo(models.Model):
     tempo = models.IntegerField()
     tentativas = models.IntegerField()
     frustrado = models.BooleanField()
-    #toques = models.ManyToManyField(Toques)
+    toques = models.ManyToManyField(Toques)
     def __str__(self):
         return "Aluno: " + self.aluno.__str__() + ", Fase: " + self.fase.__str__()
-
-class Toques(models.Model):
-    x = models.IntegerField()  # ponto x
-    y = models.IntegerField()  # ponto y
-    t = models.IntegerField()  # tempo
-    acao = models.IntegerField()  # qual acao sera executada nesse clique
-    jogo = models.ForeignKey(Jogo)
-
-    def __str__(self):
-        return "x: " + self.x.__str__() + ", y:".__str__() + self.y.__str__() + ", t:" + self.t.__str__()
 
     '''{
         "fase": 1,
